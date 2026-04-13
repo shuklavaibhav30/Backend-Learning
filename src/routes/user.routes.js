f78a1878bc99b132374dd39a7df220a0a14f312a
VAIBHAV <imvksofficial@gmail.com>
2026-04-13 20:00:24 +0530

    fixed typos

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -40,7 +40,7 @@ router.route("/change-password").post(verifyJWT,changeCurrentPassword)
 router.route("/current-user").get(verifyJWT,getCurrentUser)
 router.route("/update-accounts").patch(verifyJWT,updateAccountDetails)
 router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar)
-router.route("/cover-image").patch(verifyJWT,upload.single("/coverImage"),updateCoverImage)
+router.route("/cover-image").patch(verifyJWT,upload.single("coverImage"),updateCoverImage)
 router.route("/c/:username").get(verifyJWT,getUserChannelProfile)
 router.route("/history").get(verifyJWT,getWatchHistory)
 export default router
